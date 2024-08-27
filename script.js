d0c8840d6f2a7069ac9570f028c458d1f2892fb8
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-08-27 17:13:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+
+function getHelloWorldPromise() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Hello, world!");
+        }, 1000); 
+    });
+}
+
+
+getHelloWorldPromise().then(message => {
+    document.getElementById('output').innerText = message;
+});
+
